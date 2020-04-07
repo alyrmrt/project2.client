a604fb133e1d84eec7a296d8479df5ac3a23d19c
alyrtmrt <alayurtmm@gmail.com>
2020-04-07 16:48:16 -0400

    fixed production url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://strawberry-pie-53189.herokuapp.com/',
+  production:  'https://strawberry-pie-53189.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
